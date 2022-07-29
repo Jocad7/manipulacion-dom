98546eaa6b1f3e29ccde1aa4784657e2622e4f49
dan <valiantd7@gmail.com>
2022-07-29 01:22:34 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+console.log('Jeremy Bearimy')
+
+const h1 = document.querySelector('h1');
+const input1 = document.querySelector('#calculo1');
+const input2 = document.querySelector('#calculo2');
+
+const button = document.querySelector('#btnCalcular');
+
+
+
