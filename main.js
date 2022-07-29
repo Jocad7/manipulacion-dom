64878ba019139b10da84e6bc7b75032d1c8de08b
dan <valiantd7@gmail.com>
2022-07-29 01:38:02 -0400

    update js file

--- main.js
+++ main.js
@@ -5,6 +5,12 @@ const input1 = document.querySelector('#calculo1');
 const input2 = document.querySelector('#calculo2');
 
 const button = document.querySelector('#btnCalcular');
+const result = document.querySelector('p');
 
+button.addEventListener('click', sumValues);
 
+function sumValues(){
+    result.innerHTML = 
+        "El resultado es: " + (Number(input1.value) + Number(input2.value));
+}
 
